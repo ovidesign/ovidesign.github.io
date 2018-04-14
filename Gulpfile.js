2be8857e02e02290fedcd28fb2810c315fe49017
mikefreiman <michael@michaelfreiman.com>
2018-04-14 00:16:58 -0500

    Added Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var cleancss = require('gulp-cleancss');
+
+// Sass
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        proxy: "127.0.0.1:4000"
+    });
+
+    gulp.watch('**/*.scss', ['sass']);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("./_sass/atf.scss")
+        .pipe(sass())
+        .pipe(cleancss({keepBreaks: false}))
+        .pipe(gulp.dest("./_includes"))
+        .pipe(browserSync.stream());
+});
+
+
+gulp.task('default', ['serve']);
